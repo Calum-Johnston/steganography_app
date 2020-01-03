2c8b0c1c0eee4c4fa9c1f0939c7cd0a4e7a86d94
Calum-Johnston <calumphilipjohnston@gmail.com>
2020-01-03 14:43:10 +0000

    Added LSB and LSBM methods

--- src/com/java/calumjohnston/encoder_GUI.java
+++ src/com/java/calumjohnston/encoder_GUI.java
@@ -13,6 +13,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class encoder_GUI {
     private JPanel rootPanel;
     private JButton selectImageButton;
@@ -24,6 +26,7 @@ public class encoder_GUI {
     private BufferedImage coverImage;
     private BufferedReader textFile;
     private String text;
+    private String coverImageName;
 
 
     /**
@@ -57,59 +60,121 @@ public class encoder_GUI {
         encodeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                LSB();
+                encodeData();
             }
         });
     }
 
 
     /**
-     * FUNCTION: Performs the LSB algorithm and writes result to file
-     *           (detailed in text document: LSB.txt)
-     * INPUT: BufferedImage coverImage: The image of which data will be hidden in
-     *        String binary: The data to hide in the image
+     * FUNCTION: Determines which algorithm to apply when embedding the data
+     * INPUT: None
      * RETURN: None
      *
      * NOTES: None
      * **/
-    public void LSB(){
-        // Get binary equivalent of text to hide
-        StringBuilder binary = getBinaryData(textField.getText());
+    public void encodeData(){
+        // Converts text to be hidden into it's binary equivalent
+        StringBuilder binaryText = getBinaryText(textField.getText());
 
-        int currentPosition = 0;
+        // Calls algorithm to embed the data
+        LSBM(coverImage, binaryText);
+    }
+
+    // ====== ALGORITHMS ======
+    /**
+     * FUNCTION: Performs the LSB algorithm
+     * INPUT: BufferedImage coverImage: The image of which data will be hidden in
+     *        StringBuilder binaryText: The binary data to be hidden within the image
+     * RETURN: None
+     *
+     * NOTES: Algorithm detailed in LSB.txt
+     * **/
+    public void LSB(BufferedImage coverImage, StringBuilder binaryText){
+        // Initialise starting image pixel position
         int x = 0; int y = 0;
-        while(currentPosition < binary.length()){
-            // Get current pixel data
+        char data;
+
+        for(int pos = 0; pos < binaryText.length(); pos++){
+            // Get message data from text at a specific point
+            data = binaryText.charAt(pos);
+
+            // Get pixel data from image at a specific location
             int pixel = coverImage.getRGB(x, y);
             int red = (pixel & 0x00ff0000) >> 16;
             int green = (pixel & 0x0000ff00) >> 8;
             int blue = pixel & 0x000000ff;
 
-            // Manipulate data here
+            // Manipulate pixel data (only writes to Green LSB currently)
             String binaryGreen = Integer.toBinaryString(green);
-            String newGreen = binaryGreen.substring(0, binaryGreen.length() - 1) + binary.charAt(currentPosition);
-            green = Integer.parseInt(newGreen, 2);
+            String updatedGreen = binaryGreen.substring(0, binaryGreen.length() - 1) + data;
+            green = Integer.parseInt(updatedGreen, 2);
 
-            // Update current pixel data
+            // Write new pixel data to image at specified location
             Color newColour = new Color(red, green, blue);
             int newRGB = newColour.getRGB();
             coverImage.setRGB(x, y, newRGB);
 
-            // Update position
-            currentPosition += 1;
+            // Update position in image to manipulate pixel
             x += 1;
             if(x == coverImage.getWidth()){
-                x = 0;
-                y += 1;
+                x = 0; y += 1;
             }
         }
 
-        try{
-            File outputFile = new File("Result.png");
-            ImageIO.write(coverImage, "jpg", outputFile);
-        }catch(Exception e){
-            e.printStackTrace();
+        // Call a function to write image to disk
+        writeImageFile(coverImage);
+
+    }
+
+    /**
+     * FUNCTION: Performs the LSBM algorithm
+     * INPUT: BufferedImage coverImage: The image of which data will be hidden in
+     *        StringBuilder binaryText: The binary data to be hidden within the image
+     * RETURN: None
+     *
+     * NOTES: Algorithm detailed in LSBM.txt
+     * **/
+    public void LSBM(BufferedImage coverImage, StringBuilder binaryText){
+        // Initialise starting image pixel position
+        int x = 0; int y = 0;
+        char data;
+
+        for(int pos = 0; pos < binaryText.length(); pos++){
+            // Get message data from text at a specific point
+            data = binaryText.charAt(pos);
+
+            // Get pixel data from image at a specific location
+            int pixel = coverImage.getRGB(x, y);
+            int red = (pixel & 0x00ff0000) >> 16;
+            int green = (pixel & 0x0000ff00) >> 8;
+            int blue = pixel & 0x000000ff;
+
+            // Manipulate pixel data (only writes to Green LSB currently)
+            String binaryGreen = Integer.toBinaryString(green);
+            // If the bit to be embedded is not equal to the LSB of the pixel (Green)
+            if(!(binaryGreen.substring(binaryGreen.length() - 1).equals(Character.toString(data)))){
+                if(ThreadLocalRandom.current().nextInt(0, 2) < 1){
+                    green -= 1;
+                }else{
+                    green += 1;
+                }
+            }
+
+            // Write new pixel data to image at specified location
+            Color newColour = new Color(red, green, blue);
+            int newRGB = newColour.getRGB();
+            coverImage.setRGB(x, y, newRGB);
+
+            // Update position in image to manipulate pixel
+            x += 1;
+            if(x == coverImage.getWidth()){
+                x = 0; y += 1;
+            }
         }
+
+        // Call a function to write image to disk
+        writeImageFile(coverImage);
 
     }
 
@@ -124,12 +189,18 @@ public class encoder_GUI {
      * NOTES: None
      * **/
     public void readImageFile(){
+        // Sets the type of file to get
         openFileChooser.setFileFilter(new FileNameExtensionFilter("PNG images", "png"));
         int returnValue = openFileChooser.showOpenDialog(null);
+
         if(returnValue == JFileChooser.APPROVE_OPTION){
             try{
                 // Get image based from file explorer
                 coverImage = ImageIO.read(openFileChooser.getSelectedFile());
+
+                // Store image name (ensuring to remove extension)
+                coverImageName = openFileChooser.getSelectedFile().getName();
+                coverImageName = coverImageName.replaceFirst("[.][^.]+$", "");
 
                 // User can now encode
                 encodeButton.setEnabled(true);
@@ -138,6 +209,7 @@ public class encoder_GUI {
                 System.out.println("Successfully read in image");
 
             }catch(IOException e){
+                e.printStackTrace();
                 System.out.println("Failed to read in image");
             }
         }
@@ -152,8 +224,10 @@ public class encoder_GUI {
      * NOTES: None
      * **/
     public void readTextFile(){
+        // Sets the type of file to get
         openFileChooser.setFileFilter(new FileNameExtensionFilter("Text files", "txt"));
         int returnValue = openFileChooser.showOpenDialog(null);
+
         if(returnValue == JFileChooser.APPROVE_OPTION){
             try{
                 // Reads in text file selected in file explorer
@@ -167,10 +241,11 @@ public class encoder_GUI {
                 textField.setText(text);
 
                 // Debugging purposes
-                System.out.println("Success");
+                System.out.println("Successfully read in text");
 
             }catch(Exception e){
-                System.out.println("Fail");
+                e.printStackTrace();
+                System.out.println("Failed to read in text");
             }
         }
     }
@@ -183,7 +258,7 @@ public class encoder_GUI {
      *
      * NOTES: Currently only uses 7 bit ASCII, can update at later stage
      * **/
-    public StringBuilder getBinaryData(String text){
+    public StringBuilder getBinaryText(String text){
         byte[] bytes = text.getBytes();
         StringBuilder binary = new StringBuilder();
         for(byte b : bytes){
@@ -192,6 +267,29 @@ public class encoder_GUI {
             binary.append(formatted);
         }
         return binary;
+    }
+
+
+    /**
+     * FUNCTION: Writes in a image file to the disk
+     * INPUT: BufferedImage image: Image to write to file
+     * RETURN: None
+     *
+     * NOTES: None
+     * **/
+    public void writeImageFile(BufferedImage image){
+        try{
+            // Writes file to the disk (w/extension of algorithm used)
+            File outputFile = new File("rsts/LSB_" + coverImageName + ".png");
+            ImageIO.write(coverImage, "png", outputFile);
+
+            // Debugging purposes
+            System.out.println("Successfully written file to disk");
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Failed to write file to disk");
+        }
     }
 
 
