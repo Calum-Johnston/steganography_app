0a07ceb7836d5e89fbf45b7dc876e84f775f9a35
Calum-Johnston <calumphilipjohnston@gmail.com>
2020-01-06 11:26:02 +0000

    Added a PRNG to the program

---
+++ src/com/java/calumjohnston/randomgenerators/pseudorandom.java
@@ -0,0 +1,70 @@
+package com.java.calumjohnston.randomgenerators;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * The class produces sequence of unique numbers within a set
+ * range based off a key.
+ * The key is some user input (typically a password).
+ *
+ * Designed for Image Steganography
+ */
+public class pseudorandom{
+
+    int height;
+    int width;
+    String seed;
+    List<Integer> orderSequence;
+    int position;
+
+    /**
+     * Secondary Constructor for the class
+     * Only called when no user-input given
+     *
+     * @param height    the height of the image
+     * @param width     the width of the image
+     */
+    public pseudorandom(int height, int width) {
+        this(height, width, "default");
+    }
+
+    /**
+     * Primary Constructor for the class
+     *
+     * @param height    the height of the image
+     * @param width     the width of the image
+     * @param seed      the seed to be used for random generation
+     */
+    public pseudorandom(int height, int width, String seed){
+        this.height = height;
+        this.width = width;
+        this.seed = seed;
+        orderSequence = new ArrayList<Integer>();
+        position = -1;
+    }
+
+    /**
+     * Generates the random sequence to be used
+     */
+    public void generateRandomList(){
+        for(int i = 0; i < width * height; i++){
+            orderSequence.add(i);
+        }
+        // https://stackoverflow.com/questions/6284589/setting-a-seed-to-shuffle-arraylist-in-java-deterministically
+        // https://stackoverflow.com/questions/27346809/getting-a-range-off-user-input-for-random-generation
+        Collections.shuffle(orderSequence, new Random(seed.hashCode()));
+    }
+
+    /**
+     *  Gets the next element in the random sequence
+     *
+     * @return      the next element in the sequence
+     */
+    public int getNextElement(){
+        position += 1;
+        return orderSequence.get(position);
+    }
+}
